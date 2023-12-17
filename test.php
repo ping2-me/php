386b954a231ead5133e4ce0954291555a83f133d
bangnokia <bangnokia@gmail.com>
2023-12-18 02:19:48 +0700

    update readme

--- test.php
+++ test.php
@@ -2,6 +2,20 @@
 
 include './vendor/autoload.php';
 
-//\Ping2Me\Php\Ping::$endpoint = '@daudau/debug';
+/**
+ * ------------------------------------------------------------
+ * This is the test file for the package.
+ * ------------------------------------------------------------
+ *
+ * To test the package, run this file in your terminal:
+ * `php test.php`
+ *
+ * Why we don't use PHPUnit? Because we want to test the package
+ * in a real environment, not in a testing environment. And
+ * the package is just so simple, so we don't want to do
+ *
+ */
+
+\Ping2Me\Php\Ping::$endpoint = '@daudau/debug';
 
 ping('function working!');
